09c18282af5eeb4aae57c61ec3317a982b8cf984
Jordan Zane <jordanzane@Jordans-MacBook-Pro-2022.local>
2025-08-27 15:17:58 -0700

    Force Vercel redeploy with latest UI improvements - LeadFlow PRO

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Force Vercel redeploy with latest UI improvements - LeadFlow PRO
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
